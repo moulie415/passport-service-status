0575f0aed8f8e248e7def5cac1b369e2b1c067c7
Henry Moule <hen10@hotmail.co.uk>
2022-05-15 12:10:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+import axios from 'axios'
+import express from 'express'
+import { CourierClient } from "@trycourier/courier";
+
+ const courier = CourierClient(
+   { authorizationToken: process.env.AUTH_TOKEN });
+ 
+const app = express()
+const port = 3000
+const minutes = 1
+const the_interval = minutes * 60 * 1000;
+let error = false;
+let data;
+const emails = []
+
+const sendEmails = async (body) => {
+  Promise.all(emails.map(email => {
+    return  courier.send({
+      message: {
+        content: {
+          title: "Passport appointment service",
+          body,
+        },
+        to: {
+          email
+        }
+      }
+    });
+  }))
+  // const { requestId } = await courier.send({
+  //   message: {
+  //     content: {
+  //       title: "Passport appointment service",
+  //       body: "Want to hear a joke? {{joke}}"
+  //     },
+  //     // data: {
+  //     //   joke: "Why was the JavaScript developer sad? Because they didn't Node how to Express themselves"
+  //     // },
+  //     to: {
+  //       email: "hen10@hotmail.co.uk"
+  //     }
+  //   }
+  // });
+}
+
+app.listen(port, () => {
+
+  console.log(`listening at http://localhost:${port}`)
+  const getResponse = async () => {
+    try {
+      const res = await axios.get('https://www.passportappointment.service.gov.uk');
+      if (error) {
+        data = JSON.stringify(res.data);
+        await sendEmails(
+          `Website recovered from error, status : ${res.status}\n
+          data: ${data}
+        `)
+      } else if (JSON.stringify(res.data) !== data) {
+        data = JSON.stringify(res.data);
+        await sendEmails(
+          `status : ${res.status}\n
+          data: ${data}
+        `)
+      }
+      error = false;
+    } catch(e) {
+      if (!error) {
+        await sendEmails(`Error response meaning website is probably down: ${e.message}`)
+      }
+      error = true;
+      console.log(e.message)
+    }
+  }
+  getResponse();
+  setInterval(() => {
+    getResponse();
+  }, the_interval);
+})
+
